15a3a71e6b04b64a92d52bfc8fed9305d543bbc8
ferin <sudawan91@gmail.com>
2025-08-11 14:51:38 +0700

    update

--- controllers/profile.js
+++ controllers/profile.js
@@ -2,9 +2,10 @@ const renderError = require("../utils/renderError");
 
 exports.createProfile = (req, res, next) => {
   try {
-    if (true) {
-      return renderError(401, "Token invalid");
-    }
+    const { firstname, lastname } = req.body;
+    console.log(req.headers.authorization);
+    console.log(firstname, lastname);
+
     console.log("hello from profile controller");
     res.json({ message: "Profile created successfully" });
   } catch (error) {
